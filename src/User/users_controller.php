9e3bc8e0ae152c0a3cfbc64e705dbbc2aff36d43
Nicky de Vendt <nicky@sensimedia.nl>
2018-03-06 16:02:48 +0100

    adding new file

---
+++ src/User/users_controller.php
@@ -0,0 +1,21 @@
+<?php
+
+class UsersController
+{
+    private $repository;
+
+    public function UsersController(IUsersRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function register($params)
+    {
+        if (isset($params['username'])) {
+        $user = new User($params['username'], $params['email'], $params['firstname'], $params['lastname'], $params['currentemployer'], $params['password']);
+        $this->repository->add($user);
+
+        return '/users/index.php';
+        }
+    }
+}
