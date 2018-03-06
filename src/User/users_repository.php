251692d09e546cde37efc5a44ee3f1ec9af3327d
Nicky de Vendt <nicky@sensimedia.nl>
2018-03-06 16:02:52 +0100

    adding new file

---
+++ src/User/users_repository.php
@@ -0,0 +1,37 @@
+<?php
+
+interface IUsersRepository
+{
+    public function add(User $user);
+}
+
+class UsersRepository implements IUsersRepository
+{
+    private static $connection_string;
+
+    public static function set_connection_string($value)
+    {
+        UsersRepository::$connection_string = $value;
+    }
+
+    private $connection;
+
+    public function UsersRepository()
+    {
+         $connection = new mysqli("127.0.0.1", "nicky", "blarps", "nicky");
+    }
+
+    public function add(User $user)
+    {
+        $statement = $this->connection->prepare('INSERT INTO "users" ("firstname", "lastname", "email", "currentemployer", "username", "password") VALUES (?, ?, ?, ?, ?, ?)');
+        $statement->bind_param("firstname", $user->get_firstname());
+        $statement->bind_param("lastname", $user->get_lastname());
+        $statement->bind_param("email", $user->get_email());
+        $statement->bind_param("currentemployer", $user->get_currentemployer());
+        $statement->bind_param("username", $user->get_username());
+        $statement->bind_param("password", $user->get_password());
+
+        if (!$statement->execute())
+            throw new Exception("Execute failed: ({$stmt->errno}) {$stmt->error}");
+        }
+}
