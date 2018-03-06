a98c5321073e2382948b556dcd7295e5e21ed166
Nicky de Vendt <nicky@sensimedia.nl>
2018-03-06 16:02:18 +0100

    adding new file

---
+++ info/droptable.sql
@@ -0,0 +1,12 @@
+DROP TABLE users;
+DROP TABLE visitors;
+DROP TABLE employers;
+DROP TABLE projects;
+DROP TABLE affiliatedcompanys;
+
+DROP SEQUENCE users_id_seq;
+DROP SEQUENCE visitors_id_seq;
+DROP SEQUENCE employers_id_seq;
+DROP SEQUENCE projects_id_seq;
+DROP SEQUENCE affiliatedcompanys_id_seq;
+
