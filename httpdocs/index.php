c93b13a7fa0ffee97d40a39d85ac34ca71adb86b
Nicky de Vendt <nicky@sensimedia.nl>
2018-03-07 16:27:18 +0100

    fixing template

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -9,19 +9,36 @@
     ));
     $twig->addExtension(new Twig_Extension_Debug());
 
-    //var_dump($_SERVER['REQUEST_URI']);
+    ($_SERVER['REQUEST_URI']);
 
     switch ($_SERVER['REQUEST_URI']) {
-        //$template = substr($_SERVER['REQUEST_URI'], 1);
+       // $template = substr($_SERVER['REQUEST_URI'], 1);
         case '/register':
             include '../src/User/register.php';
             $template = 'register.html.twig';
-            echo $twig->render($template, ['piemel' => $piemel]);
+            echo $twig->render($template, ['post' => $post, 'newuser' => $newuser]);
             break;
         case '/login':
             $template = 'reglog.html.twig';
             echo $twig->render($template);
             break;
+        case '/workprojects':
+            $template = 'workhistory.html.twig';
+            echo $twig->render($template);
+            break;
+        case '/about':
+            $template = 'about.html.twig';
+            echo $twig->render($template);
+            break;
+        case '/resume':
+            $template = 'resume.html.twig';
+            echo $twig->render($template);
+            break;
+        case '/contact':
+            $template = 'contact.html.twig';
+            echo $twig->render($template);
+            break;
+
         default: 
             $template = 'page.html.twig';
             echo $twig->render($template);
