c6ec40ac4c219504f902891fe16e2c2d1e0dbd3c
Nicky de Vendt <nicky@sensimedia.nl>
2018-03-08 17:00:33 +0100

    database query handling for registering user

--- src/User/register.php
+++ src/User/register.php
@@ -1,10 +1,34 @@
 <?php
 
-include 'users_controller.php';
+$data = array(
+    $_REQUEST['firstname'],
+    $_REQUEST['lastname'],
+    $_REQUEST['email'],
+    $_REQUEST['currentemployer'],
+    $_REQUEST['username'],
+    $_REQUEST['password']
+    );
+if (isset($_REQUEST['submit']))
+{
+$result = insert($data);
+}
 
-$message = 'success!';
-$post = $_POST;
-
-
-?>
+function insert ($data)
+{
+    try {
+        $pdo = new PDO('pgsql:host=localhost;dbname=nicky;', 'nicky', 'blarps');
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $statement = $pdo->prepare('INSERT INTO users (firstname, lastname, email, currentemployer, username, password) VALUES (?, ?, ?, ?, ?, ?)');
+        $statement->execute($data);
+        if (null !== $statement->fetch(PDO::FETCH_ASSOC))
+        {
+            return true;
+        }
+        else {
+            return false;
+        }
+    } catch (PDOException $e) {
+        die('error!: ' . $e->getMessage() );
+    }
+}
 
