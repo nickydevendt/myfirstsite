d6673b456db2f8fe91574c4964e59e930eb39534
Nicky de Vendt <nicky@sensimedia.nl>
2018-03-26 17:20:34 +0200

    userpanel email service for adding new visitor is not working yet so its commented out but needs fixing in the next couple of days

--- src/User/userpanel.php
+++ src/User/userpanel.php
@@ -24,7 +24,7 @@ function getCurrentUser() : array{
             $statement->execute(array($_SESSION['userid']));
             return $statement->fetch(PDO::FETCH_ASSOC);
         } else {
-    //        echo "<script>alert('You are not logged in or are doing shady stuff you are redirected!');document.location='/login'</script>";
+            echo "<script>alert('You are not logged in or are doing shady stuff you are redirected!');document.location='/login'</script>";
             session_destroy();
         }
     } catch (PDOException $e) {
@@ -67,7 +67,7 @@ function getMyVisitors() : array {
 
 function checkLogin() {
     if(!isset($_SESSION['admin']) || !isset($_SESSION['userid'])) {
-  //      echo "<script>alert('You are not logged in and you are redirected!');document.location='/login'</script>";
+        echo "<script>alert('You are not logged in and you are redirected!');document.location='/login'</script>";
     }
 }
 
@@ -100,13 +100,42 @@ function addVisitor($inviteid, $firstname, $lastname, $email) {
     try{
         $statement = $pdo->prepare('INSERT INTO visitors (inviteid, firstname, lastname, email) VALUES(?,?,?,?)');
         $statement->execute(array($inviteid, $firstname, $lastname, $email));
+        $lastid = $pdo->lastInsertId();
+/*
+        if(isset($lastid)) {
+            $stmt = $pdo->prepare('SELECT email,randomid FROM visitors where id = ?');
+            $stmt->execute(array($lastid));
+            $value = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            var_dump($value['email']);
+            $email = $value['email'];
+            $randomid = $value['randomid'];
+            die();
+
+            if(isset($email) && !empty($email) && isset($randomid) && !empty($randomid)) {
+                emailNewVisitor($email, $randomid);
+            }
+        }
+        */
     } catch (PDOException $e) {
         die('error!: ' . $e->getMessage());
     }
 }
-
+/*
+function emailNewVisitor($email, $randomid) {
+    $to = $email;
+    $subject = "Your Visitor code";
+    $message = 'Welcome you are invited to My first website I would really like it if you gave it a look your code is: ' .$randomid . 'you can use this on the login page and scroll down where there is a personalize section for your visitor code!';
+    $headers = array(
+        'from' => 'Nickydevendt@hotmail.com',
+        'Reply-To' => 'nickydevendt@hotmail.com',
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+    mail($to,$subject,$message, $headers);
+}
+*/
 function redirect() {
-//    header( "refresh:0;url=/login" );
+    header( "refresh:0;url=/login" );
     session_destroy();
 }
 
